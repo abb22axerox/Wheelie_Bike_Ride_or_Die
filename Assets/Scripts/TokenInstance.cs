7a8217244ff8190210277021c5b7fe62cb8af8cf
Noah <Noah.Marklund0@gmail.com>
2024-09-27 18:54:29 +0200

    sm

--- Assets/Scripts/TokenInstance.cs
+++ Assets/Scripts/TokenInstance.cs
@@ -1,15 +1,15 @@
-using UnityEngine;
+// using UnityEngine;
 
-public class TokenInstance : MonoBehaviour
-{
-    void OnPlayerEnter(PlayerController player)
-    {
-        RemoveToken(player);
+// public class TokenInstance : MonoBehaviour
+// {
+//     void OnPlayerEnter(PlayerController player)
+//     {
+//         RemoveToken(player);
 
-        ScoreManager.instance.AddPoint();
-    }
+//         ScoreManager.instance.AddPoint();
+//     }
 
-    void Awake();
-    void OnTriggerEnter2D(Collider2D other);
-    void RemoveToken(PlayerController player);
-}
+//     void Awake();
+//     void OnTriggerEnter2D(Collider2D other);
+//     void RemoveToken(PlayerController player);
+// }
