5cc6df218392d39e64c151ca5f60554d2697f28a
abbwilemi <william.emilsson@abbgymnasiet.se>
2024-09-27 20:02:44 +0200

    Big Commit

--- Assets/Scripts/GameOverScreen.cs
+++ Assets/Scripts/GameOverScreen.cs
@@ -1,10 +1,11 @@
-using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
+using System.Collections;
+// using UnityEngine.UI;
+using TMPro;
 
 public class GameOverScreen : MonoBehaviour
 {
-    public Text pointText;
+    public TextMeshProUGUI pointText;
 
     public void Setup(int score)
     {
