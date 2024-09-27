d0222511f4240ffff6521ddd6a57ef3e462612a2
abb22axerox <axel.roxenborg@abbgymnasiet.se>
2024-09-27 19:04:09 +0200

    pushypushy

--- Assets/Scripts/Highscore_and_Coins.cs
+++ Assets/Scripts/Highscore_and_Coins.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,21 +9,40 @@ public class ScoreManager : MonoBehaviour {
 
     public Text scoreText;
     public Text highscoreText;
+    public Text coinText;  // New Text component for displaying the coin count
 
     [NonSerialized] public int score = 0;
     int highscore = 0;
+    int coinCount = 0;  // Variable to keep track of the number of coins
 
     private void Awake() {
         instance = this;
     }
 
     void Start () {
+        if (scoreText == null || highscoreText == null || coinText == null) return;
+
         scoreText.text = score.ToString() + " POINTS";
-        highscoreText.text = "HIGHSCORE" + highscore.ToString();
+        highscoreText.text = "HIGHSCORE: " + highscore.ToString();
+        coinText.text = "COINS: " + coinCount.ToString();  // Initialize the coin text
     }
 
     public void AddPoints (int newPoints) {
+        if (scoreText == null || highscoreText == null) return;
+
         score += newPoints;
         scoreText.text = score.ToString() + " POINTS";
+
+        if (score > highscore) {
+            highscore = score;
+            highscoreText.text = "HIGHSCORE: " + highscore.ToString();
+        }
+    }
+
+    public void AddCoin(int newCoins) {
+        if (coinText == null) return;
+
+        coinCount += newCoins;
+        coinText.text = "COINS: " + coinCount.ToString();  // Update the coin text
     }
 }
