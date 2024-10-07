d4b4727a0b322eb4bf5995103de673e206f3299a
abbwilemi <william.emilsson@abbgymnasiet.se>
2024-10-07 17:09:36 +0200

    updated shop buttons

--- Assets/Scripts/Keybind.cs
+++ Assets/Scripts/Keybind.cs
@@ -1,37 +1,28 @@
 using UnityEngine;
 using TMPro;
 using System;
- 
+
 public class Keybind : MonoBehaviour
 {
-    [Header("Objects")]
-    [SerializeField] private TextMeshProUGUI button1; // Button 1 UI text
-    [SerializeField] private TextMeshProUGUI button2; // Button 2 UI text
-    [SerializeField] private TextMeshProUGUI button3; // Button 3 UI text
-    [SerializeField] private TextMeshProUGUI button4; // Button 4 UI text
- 
-    private TextMeshProUGUI currentButton; // To track which button is being changed
- 
-    // Start is called before the first frame update
+    [SerializeField] private TextMeshProUGUI button1;
+    [SerializeField] private TextMeshProUGUI button2;
+    [SerializeField] private TextMeshProUGUI button3;
+    [SerializeField] private TextMeshProUGUI button4;
+
+    private TextMeshProUGUI currentButton;
+
     private void Start()
     {
-        // Load saved keys or set default values for each button
         if (button1 != null)
-            button1.text = PlayerPrefs.GetString("CustomKey1", "w");
+            button1.text = PlayerPrefs.GetString("ForwardButton", "w");
         if (button2 != null)
-            button2.text = PlayerPrefs.GetString("CustomKey2", "s");
+            button2.text = PlayerPrefs.GetString("BackwardButton", "s");
         if (button3 != null)
-            button3.text = PlayerPrefs.GetString("CustomKey3", "a");
+            button3.text = PlayerPrefs.GetString("LeftButton", "a");
         if (button4 != null)
-            button4.text = PlayerPrefs.GetString("CustomKey4", "d");
- 
-        PlayerPrefs.SetString("ForwardButton", "w");
-        PlayerPrefs.SetString("BackwardButton", "s");
-        PlayerPrefs.SetString("LeftButton", "a");
-        PlayerPrefs.SetString("RightButton", "d");
+            button4.text = PlayerPrefs.GetString("RightButton", "d");
     }
- 
-    // Update is called once per frame
+
     private void Update()
     {
         if (currentButton != null && currentButton.text == "Press Key")
@@ -41,63 +32,57 @@ public class Keybind : MonoBehaviour
                 if (Input.GetKeyDown(keycode))
                 {
                     currentButton.text = keycode.ToString();
- 
-                    // Save the key to the corresponding PlayerPrefs based on which button is being changed
+
                     if (currentButton == button1)
-                        PlayerPrefs.SetString("ForwardButton", keycode.ToString() == "UpArrow" ? "up" : keycode.ToString().ToLower());
+                        PlayerPrefs.SetString("ForwardButton", keycode.ToString().ToLower());
                     else if (currentButton == button2)
-                        PlayerPrefs.SetString("BackwardButton", keycode.ToString() == "DownArrow" ? "down" : keycode.ToString().ToLower());
+                        PlayerPrefs.SetString("BackwardButton", keycode.ToString().ToLower());
                     else if (currentButton == button3)
-                        PlayerPrefs.SetString("LeftButton", keycode.ToString() == "LeftArrow" ? "left" : keycode.ToString().ToLower());
+                        PlayerPrefs.SetString("LeftButton", keycode.ToString().ToLower());
                     else if (currentButton == button4)
-                        PlayerPrefs.SetString("RightButton", keycode.ToString() == "RightArrow" ? "right" : keycode.ToString().ToLower());
+                        PlayerPrefs.SetString("RightButton", keycode.ToString().ToLower());
+
                     PlayerPrefs.Save();
-                    currentButton = null; // Reset after a key is assigned
+                    currentButton = null;
                     break;
                 }
             }
         }
     }
- 
-    // Method to change the key for button 1
+
     public void ChangeKey1()
     {
         if (button1 != null)
         {
             button1.text = "Press Key";
-            currentButton = button1; // Set the current button to button1
+            currentButton = button1;
         }
     }
- 
-    // Method to change the key for button 2
+
     public void ChangeKey2()
     {
         if (button2 != null)
         {
             button2.text = "Press Key";
-            currentButton = button2; // Set the current button to button2
+            currentButton = button2;
         }
     }
- 
-    // Method to change the key for button 3
+
     public void ChangeKey3()
     {
         if (button3 != null)
         {
             button3.text = "Press Key";
-            currentButton = button3; // Set the current button to button3
+            currentButton = button3;
         }
     }
- 
-    // Method to change the key for button 4
+
     public void ChangeKey4()
     {
         if (button4 != null)
         {
             button4.text = "Press Key";
-            currentButton = button4; // Set the current button to button4
+            currentButton = button4;
         }
     }
 }
- 
- 
